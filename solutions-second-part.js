c718332f5ae24d7c2b7504ec52876e16be108226
Andrew Prymak <101932074+PrymakAndrii@users.noreply.github.com>
2026-04-27 10:12:14 +0000

    Add new solution

--- solutions-second-part.js
+++ solutions-second-part.js
@@ -16,3 +16,15 @@ function giveMeFive(obj) {
   }
   return result;
 }
+
+//Understanding closures - the basics
+function buildFun(n) {
+  let res = [];
+
+  for (let i = 0; i < n; i++) {
+    res.push(function () {
+      return i;
+    });
+  }
+  return res;
+}
